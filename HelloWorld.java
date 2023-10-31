a4e24718979d853584acb42b265e8ba8a7380191
kalimuthuvellaichamy <149378582+kalimuthuvellaichamy@users.noreply.github.com>
2023-10-31 12:48:20 +0530

    Update HelloWorld.java
    kali updated the code for testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ * Kali update for testing
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World from Oracle DevOps create on Oct 31, 2023");
+    System.out.println("Hello, World from Oracle DevOps create on Nov 31, 2023");
   }
 }
